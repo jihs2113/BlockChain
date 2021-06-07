eb1c1c8e14791757600a0dc3f297c91dd40ff5c4
Terry_Kim <48509331+jihs2113@users.noreply.github.com>
2021-06-07 22:44:58 +0900

    watch & src setting

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// const name = "Jihwan",
+//     age = 24,
+//     gender = "male";
+
+const sayHi = (name:string, age:number, gender:string):string =>{
+    return (`Hello ${name}, you are ${age}, you r a ${gender}!`);
+};
+
+sayHi("hwan", 22, "male");
+
+export {};
