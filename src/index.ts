9e0bea4d666bf7a637e8df6b69e7016541a647ce
Terry_Kim <48509331+jihs2113@users.noreply.github.com>
2021-06-10 02:43:32 +0900

    first created block

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,32 @@
-interface Human {
-//인터페이스는 JS에선 움직이지 않고type에서만 움직임.
-//블록체인의 경우 하나의 block을 인터페이스로 할수있음.   
-//그블록이 가져야되는 모든세부설명과 함께 한블록을
-//인터페이스로 정의해준다.type에서만 가능하다.    
-    name: string,
-    age: number,
-    gender: string
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+    //block structure부분이 생성되었다.
 }
 
-const person ={
-    name : "Jihwan",
-    age : 24,
-    gender : "male"
-};
+const genesisBlock: Block = new Block(0, "231232323", "", "Hello", 123456);
 
-// const sayHi = (name:string, age:number, gender:string):string =>{
-//     return (`Hello ${name}, you are ${age}, you r a ${gender}!`);
-// };
 
-const sayHi = (person: Human):string =>{
-    //위에처럼 따로 파싱하지않고 
-    return (`Hello ${person.name}, you are ${person.age}, you r a ${person.gender}!`);
-};
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(person));
-//person인 오브젝트를 보냈을때 human 인터페이스와
-//같은 구조인지를 함수의 pasing을 통해서 보고
+console.log(blockchain);
 
-export {};
+
+export{};
